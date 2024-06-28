f7bc846dbb154ec6dc69b15844832e837bce30b5
galzmarc <marco.galzignato@gmail.com>
2024-06-28 14:55:27 +0100

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -35,9 +35,7 @@ impl ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
-
         let receiver = Arc::new(Mutex::new(receiver));
-
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
